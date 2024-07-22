6b8f241d229cbd17c954e02005625dd7f6b8ca5a
Jemutai <janetjemutai19@gmail.com>
2024-07-22 17:08:50 +0300

    api

---
+++ app/src/main/java/com/jemutai/globalgist/remote/NewaApi.kt
@@ -0,0 +1,23 @@
+package com.jemutai.globalgist.remote
+
+import com.jemutai.globalgist.models.NewsResponse
+import com.jemutai.globalgist.util.Constants.Companion.API_KEY
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface NewsApi {
+
+    @GET("/v2/top-headlines")
+     suspend fun  fetchAllArticles(
+        @Query("country") countryCode :String ="ke",
+        @Query("pageNumber") page :Int = 1,
+        @Query("apikey") apikey :String =API_KEY
+
+     ):Response<NewsResponse>
+
+@GET("/v2/everything")
+     suspend fun  serachNews(
+         @Query("")
+     )
+}
---
+++ app/src/main/java/com/jemutai/globalgist/util/Constants.kt
@@ -0,0 +1,11 @@
+package com.jemutai.globalgist.util
+
+class Constants {
+
+    companion object{
+        const val  API_KEY = "8cd3f675f049458f9e2deeb10b2bc3c9"
+        const val  BASE_URL = "https://newsapi.org/"
+        const val  SEARCH_NEWS_TIME_DELAY = "500L"
+        const val  QUERY_PAGE_SIZE = "20"
+    }
+}
